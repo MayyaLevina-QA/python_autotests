7df358880ebdc2d215193949b4b1a4d32428bdb3
MayyaLevina-QA <116386796+MayyaLevina-QA@users.noreply.github.com>
2022-12-08 22:54:39 +0300

    Update main.py

--- Python_project/main.py
+++ Python_project/main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-token = '78218eb6fe7b194ab8b51363e6c2bcf9'
+token = 'Здесь будет токен'
 response = requests.post('https://pokemonbattle.me:5000/pokemons', headers={'trainer_token' : token}, json={
     "name": "Лёлик",
     "photo": "https://static.wikia.nocookie.net/pokemon/images/2/21/001Bulbasaur.png"
@@ -23,19 +23,3 @@ response_pokebol = requests.post('https://pokemonbattle.me:5000/trainers/addPoke
 })
 
 print(response_pokebol.text)
-
-response_put = requests.put('https://pokemonbattle.me:5000/pokemons', headers={'trainer_token' : token}, json={
-    "pokemon_id": 1487,
-    "name": "Биба",
-    "photo": ""
-})
-
-print(response_put.text)
-
-response_put = requests.put('https://pokemonbattle.me:5000/pokemons', headers={'trainer_token' : token}, json={
-    "pokemon_id": 1488,
-    "name": "Боба",
-    "photo": ""
-})
-
-print(response_put.text)
